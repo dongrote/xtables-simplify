0dacb587b7a48576951bc2e1bd76717da8d8e35f
Don Grote <dgrote@tacnetsol.com>
2015-10-15 15:07:54 -0400

    Added 'NOTRACK' reserved target for raw iptable

--- xtables-simplify.py
+++ xtables-simplify.py
@@ -21,7 +21,7 @@ class PrimaryChain(Chain):
         return '%s %s -- %r' % (self.name, self.policy, self.rules)
 
 class XTable(object):
-    reserved_targets = ('ACCEPT', 'DROP', 'RETURN', 'DNAT', 'CONNMARK', 'NFQUEUE', 'REDIRECT', 'REJECT', ' ', 'TCPMSS')
+    reserved_targets = ('ACCEPT', 'DROP', 'RETURN', 'DNAT', 'CONNMARK', 'NFQUEUE', 'REDIRECT', 'REJECT', ' ', 'TCPMSS', 'NOTRACK')
     def __init__(self, primary_chains=[], extension_chains=[]):
         self.primary_chains = primary_chains
         self.extension_chains = extension_chains
